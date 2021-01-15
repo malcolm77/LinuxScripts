df6b41c479d8bdeaefb88b88a1a1f218a467fbf8
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2021-01-16 09:23:19 +1100

    updates and corrections

--- make_local_repo.sh
+++ make_local_repo.sh
@@ -9,11 +9,16 @@ mkdir -p /mnt/dvd
 mount /dev/sr0 /mnt/tmp
 
 # copy contents (need to check this)
-cp -r /mnt/tmp/* /mnt/dvd/
+cp -r /mnt/tmp/* /mnt/dvd
 
 # umount DVD
 umount /mnt/tmp
 
 # create local repo file
 echo "[local-dvd-repo]" > /etc/yum.repos.d/local.repo
-echo "
+echo "name=Local copy of DVD" > /etc/yum.repos.d/local.repo
+echo "gpgcheck=0" > /etc/yum.repos.d/local.repo
+echo "baseurl=file:///mnt/dvd" > /etc/yum.repos.d/local.repo
+
+# check repolist is working
+yum repolist
