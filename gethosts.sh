d321fdbe3b9b1969548122cb35affc5aa551d3c9
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2021-01-17 13:20:41 +1100

    Update gethosts.sh

--- gethosts.sh
+++ gethosts.sh
@@ -1,3 +1,2 @@
 # script/command to get master hosts file from git hub
-cp -f /etc/hosts /etc/hosts.old; wget https://raw.githubusercontent.com/malcolm77/LinuxScripts/master/hosts -O /etc/hosts
-
+cp -y /etc/hosts /etc/hosts.old; wget https://raw.githubusercontent.com/malcolm77/LinuxScripts/master/hosts -O /etc/hosts
